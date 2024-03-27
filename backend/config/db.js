6ab7df5e3601f7835b1cbd7de029b10f662e8407
asar502 <aaquibm502@gmail.com>
2024-03-27 14:25:07 +0530

    mongo db url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      'mongodb+srv://aaquibm502:Aaquib%40123@cluster0.zkrl98d.mongodb.net/tes'
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
